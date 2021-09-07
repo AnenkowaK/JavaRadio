cc34ab4c47338dc8599a5a3ca612f7ad51917663
Ekaterina Smetanina <AnenkowaK@mail.ru>
2021-09-07 18:51:25 +0300

    написаны тесты

--- src/main/java/ru/netology/radioservice/Radio.java
+++ src/main/java/ru/netology/radioservice/Radio.java
@@ -1,4 +1,36 @@
 package ru.netology.radioservice;
 
 public class Radio {
+
+    int currentVolume;
+    int currentStation;
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+
+    public void nextStation() {
+        if (currentStation > 9) {
+            currentStation = 0;
+        } else {
+            currentStation = currentStation + 1;
+        }
+    }
+
+    public void previousStation() {
+        if (currentStation <= 0) {
+            currentStation = 9;
+        } else {
+            currentStation = currentStation - 1;
+        }
+    }
 }
+
--- src/test/java/ru/netology/radioservice/RadioTest.java
+++ src/test/java/ru/netology/radioservice/RadioTest.java
@@ -1,2 +1,64 @@
-package ru.netology.radioservice;public class RadioTest {
+package ru.netology.radioservice;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+
+    @Test
+    public void shouldCreate() {
+        Radio radio = new Radio();
+    }
+
+    @Test
+    public void increaseVolume() {
+        Radio radio = new Radio();
+        radio.currentVolume = 8;
+        radio.increaseVolume();
+        assertEquals(9, radio.currentVolume);
+    }
+
+    @Test
+    public void decreaseVolume() {
+        Radio radio = new Radio();
+        radio.currentVolume = 5;
+        radio.decreaseVolume();
+        assertEquals(4, radio.currentVolume);
+    }
+
+    @Test
+    public void nextStation() {
+        Radio radio = new Radio();
+        radio.currentStation = 10;
+        radio.nextStation();
+        assertEquals(0, radio.currentStation);
+    }
+
+    @Test
+    public void previousStation() {
+        Radio radio = new Radio();
+        radio.currentStation =0;
+        radio.previousStation();
+        assertEquals(9, radio.currentStation);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
